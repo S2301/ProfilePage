1272a05480df9aa6566978c087ba38fb3c65d3dc
S2301 <84118335+S2301@users.noreply.github.com>
2023-02-02 12:10:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if ($remarks==null and $remarks=="") {
 echo ' <div id="reg-head" class="headrg">Register Here</div> ';
 }
 if ($remarks=='success') {
-echo ' <div id="reg-head" class="headrg">Registration Success</div> ';
+echo ' <div id="reg-head" class="headrg">Registration is Successful</div> ';
 }
 if ($remarks=='failed') {
 echo ' <div id="reg-head-fail" class="headrg">Registration has Failed!, Username exists</div> ';
